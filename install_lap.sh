dcf75cca17c774d9bb1af0b330e5ca11433d9767
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-23 02:39:08 -0400

    Update install_lap.sh

--- install_lap.sh
+++ install_lap.sh
@@ -2,24 +2,18 @@
 
 # install Apache and PHP (in a loop because a lot of installs happen
 # on VM init, so won't be able to grab the dpkg lock immediately)
-until apt-get -y update && apt-get -y install apache2 php5
+until apt-get -y update && apt-get -y install apache2 php5 &&	apt-get install zip unzip
 do
   echo "Try again"
   sleep 2
 done
 
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/var/www/html --filename=composer
 
 
 # write some PHP; these scripts are downloaded beforehand as fileUris
 cp index.php /var/www/html/
 cp do_work.php /var/www/html/
-cp composer.phar /var/www/html/
-cp composer.json /var/www/html/
-cd /var/www/html/ && composer install --no-dev
-composer.phar install
-composer.phar
-php composer.phar install
+unzip photos.zip -d ./var/www/html/
 chown www-data:www-data /var/www/html/*
 rm /var/www/html/index.html
 
