d994785fc7dbace7abd5d4309ebebfda4faf3bbe
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-22 22:33:07 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,45 @@
-<html>
-  <head><title>Scale Set Autoscale demo app</title></head>
-  <body>
-    <?php $hostname = gethostname(); ?>
-    <center>
-	  <h1>Scale Set App - <?php echo "$hostname";?></h1>
-	</center>
-    <br/><br/><br/>
-	<ul><ul><ul>
-	<form action="do_work.php" method="get">
-	  Duration (seconds): <input type="text" name="num" maxlength="4" size="4">
-	  <input type="submit" value="Do work">
-	</form>
-	</ul></ul></ul>
-  </body>
-</html>
+<?php
+
+echo "<h1>Hello</h1>";
+
+require_once 'vendor\autoload.php';
+use WindowsAzure\Common\ServicesBuilder;
+use WindowsAzure\Common\ServiceException;
+use WindowsAzure\ServiceBus\Models\QueueInfo;
+use WindowsAzure\ServiceBus\Models\BrokeredMessage;
+use WindowsAzure\ServiceBus\Models\ReceiveMessageOptions;
+
+
+$connectionString = "Endpoint=https://jairaj007.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=NT7d6BIJQdoPD7JW1ujKAAsfLk50jJyguSc7FYdn7Sc=";
+$serviceBusRestProxy = ServicesBuilder::getInstance()->createServiceBusService($connectionString);
+
+try    {
+    // Set the receive mode to PeekLock (default is ReceiveAndDelete).
+    $options = new ReceiveMessageOptions();
+    $options->setPeekLock();
+
+    // Receive message.
+    $message = $serviceBusRestProxy->receiveQueueMessage("myqueue", $options);
+	$obj= json_decode($message->getBody());
+    echo $obj->{'b'}."<br />";
+    echo "MessageID: ".$message->getMessageId()."<br />";
+
+    /*---------------------------
+        Process message here.
+    ----------------------------*/
+
+    // Delete message. Not necessary if peek lock is not set.
+    echo "Message deleted.<br />";
+    $serviceBusRestProxy->deleteMessage($message);
+}
+catch(ServiceException $e){
+    // Handle exception based on error codes and messages.
+    // Error codes and messages are here:
+    // http://msdn.microsoft.com/library/windowsazure/hh780735
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo $code.": ".$error_message."<br />";
+}
+
+
+?>
