222ceae95c4c3bbb3470c9aaa2b289314deb1d69
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-23 05:02:41 -0400

    Update install_lap.sh

--- install_lap.sh
+++ install_lap.sh
@@ -2,7 +2,7 @@
 
 # install Apache and PHP (in a loop because a lot of installs happen
 # on VM init, so won't be able to grab the dpkg lock immediately)
-until apt-get -y update && apt-get -y install apache2 php5 && apt install php5 && apt install curl && apt install php7.0-cli
+until apt-get -y update && apt-get -y install apache2 && apt install php5 && apt install curl && apt install php7.0-cli
 do
   echo "Try again"
   sleep 2
