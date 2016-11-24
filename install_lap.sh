468bad0a370df417859ff172a25e49212cf80afe
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-24 14:00:07 -0400

    Update install_lap.sh

--- install_lap.sh
+++ install_lap.sh
@@ -8,16 +8,12 @@ apt-get install curl php5-cli git
 apt-get install wget
 
 curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-
 # write some PHP
 cd /var/www/html
 wget https://raw.githubusercontent.com/jairaj007/Assignment3/master/index.php
 wget https://raw.githubusercontent.com/jairaj007/Assignment3/master/composer.json
 wget http://getcomposer.org/composer.phar
-curl -O https://getcomposer.org/composer.phar
 php composer.phar install       
-composer.phar install  
 rm /var/www/html/index.html
 # restart Apache
 apachectl restart
