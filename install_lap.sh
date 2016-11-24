8660fc5c823311823dc4268ee0f2fc7eb319815c
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-24 00:35:54 -0400

    Update install_lap.sh

--- install_lap.sh
+++ install_lap.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
+# wait for Linux Diagnostic Extension to complete
+while ( ! grep "Start mdsd" /var/log/azure/Microsoft.OSTCExtensions.LinuxDiagnostic/2.1.5/extension.log); do
+    sleep 5
+done
 
-# install Apache and PHP (in a loop because a lot of installs happen
-# on VM init, so won't be able to grab the dpkg lock immediately)
-apt-get -y update 
+# install Apache and PHP
+apt-get -y update
 apt-get -y install apache2 php5
-apt-get install curl
 apt-get install php7.0-cli
 
-
-
-# write some PHP; these scripts are downloaded beforehand as fileUris
-cp index.php /var/www/html/
-cp composer.json /var/www/html/
-curl -O /var/www/html http://getcomposer.org/composer.phar
-php /var/www/html/composer.phar install
-chown www-data:www-data /var/www/html/*
+# write some PHP
+cd /var/www/html
+wget https://raw.githubusercontent.com/jairaj007/Assignment3/master/index.php
+wget https://raw.githubusercontent.com/jairaj007/Assignment3/master/composer.json
+wget http://getcomposer.org/composer.phar
+php composer.phar install                    
 rm /var/www/html/index.html
-
 # restart Apache
 apachectl restart
+
