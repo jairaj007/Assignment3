507b5e7396ea171e71be79c480bf9be076f54596
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-23 21:05:01 -0400

    Update install_lap.sh

--- install_lap.sh
+++ install_lap.sh
@@ -2,17 +2,23 @@
 
 # install Apache and PHP (in a loop because a lot of installs happen
 # on VM init, so won't be able to grab the dpkg lock immediately)
-until apt-get -y update && apt-get -y install apache2 php5
+until apt-get -y update && apt-get -y install apache2 php5 && apt-get install curl
 do
   echo "Try again"
   sleep 2
 done
 
 
+
+
 # write some PHP; these scripts are downloaded beforehand as fileUris
 cp index.php /var/www/html/
-cp do_work.php /var/www/html/
+cp composer.json /var/www/html/
+curl -o /var/www/html http://getcomposer.org/composer.phar
+cd /var/www/html
+php composer.phar install
 chown www-data:www-data /var/www/html/*
 rm /var/www/html/index.html
+
 # restart Apache
 apachectl restart
