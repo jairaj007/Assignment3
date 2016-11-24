1f423f6576af2945a0fcf7fcc9258d19ab74710a
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-24 12:13:54 -0400

    Update install_lap.sh

--- install_lap.sh
+++ install_lap.sh
@@ -6,7 +6,7 @@ apt-get -y install apache2 php5
 apt-get install php7.0-cli
 apt-get install curl php5-cli git
 
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 # write some PHP
 cd /var/www/html
