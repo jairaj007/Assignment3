639b4dc6a015b07ff8d2767a34b48c85bfc4594e
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-23 20:40:20 -0400

    Update install_lap.sh

--- install_lap.sh
+++ install_lap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cp index.php /var/www/html/
+chown www-data:www-data /var/www/html/*
+rm /var/www/html/index.html
+
 # install Apache and PHP (in a loop because a lot of installs happen
 # on VM init, so won't be able to grab the dpkg lock immediately)
 until apt-get -y update && apt-get -y install apache2 && apt-get install php5 && apt-get install curl && apt-get install php7.0-cli
@@ -9,8 +13,6 @@ do
 done
 
 # write some PHP; these scripts are downloaded beforehand as fileUris
-cp index.php /var/www/html/
-chown www-data:www-data /var/www/html/*
-rm /var/www/html/index.html
+
 # restart Apache
 apachectl restart
