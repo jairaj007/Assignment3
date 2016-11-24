fdffad0995dd5f27400f29333505e5713133662a
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-24 00:46:30 -0400

    Update install_lap.sh

--- install_lap.sh
+++ install_lap.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# wait for Linux Diagnostic Extension to complete
-while ( ! grep "Start mdsd" /var/log/azure/Microsoft.OSTCExtensions.LinuxDiagnostic/2.1.5/extension.log); do
-    sleep 5
-done
-
+# wait for Linux Diagnostic Extension to complet
 # install Apache and PHP
 apt-get -y update
 apt-get -y install apache2 php5
