050bbb398c67bd0b3cf7825c95d3f559f5114721
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-24 22:08:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,45 @@
 <?php
 
+echo "<h1>Hello</h1>";
+
+require_once 'vendor\autoload.php';
+use WindowsAzure\Common\ServicesBuilder;
+use WindowsAzure\Common\ServiceException;
+use WindowsAzure\ServiceBus\Models\QueueInfo;
+use WindowsAzure\ServiceBus\Models\BrokeredMessage;
+use WindowsAzure\ServiceBus\Models\ReceiveMessageOptions;
 
 
+$connectionString = "Endpoint=https://jairaj007.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=NT7d6BIJQdoPD7JW1ujKAAsfLk50jJyguSc7FYdn7Sc=";
+$serviceBusRestProxy = ServicesBuilder::getInstance()->createServiceBusService($connectionString);
 
-echo "<h1>How Ironic</h1>";
+try    {
+    // Set the receive mode to PeekLock (default is ReceiveAndDelete).
+    $options = new ReceiveMessageOptions();
+    $options->setPeekLock();
+
+    // Receive message.
+    $message = $serviceBusRestProxy->receiveQueueMessage("myqueue", $options);
+	$obj= json_decode($message->getBody());
+    echo $obj->{'b'}."<br />";
+    echo "MessageID: ".$message->getMessageId()."<br />";
+
+    /*---------------------------
+        Process message here.
+    ----------------------------*/
+
+    // Delete message. Not necessary if peek lock is not set.
+    echo "Message deleted.<br />";
+    $serviceBusRestProxy->deleteMessage($message);
+}
+catch(ServiceException $e){
+    // Handle exception based on error codes and messages.
+    // Error codes and messages are here:
+    // http://msdn.microsoft.com/library/windowsazure/hh780735
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo $code.": ".$error_message."<br />";
+}
 
 
 ?>
