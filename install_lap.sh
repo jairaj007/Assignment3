8a72ee854a67eb48244a872aae8eba9304e31daf
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-25 18:31:08 -0400

    Update install_lap.sh

--- install_lap.sh
+++ install_lap.sh
@@ -1,9 +1,9 @@
 apt-get -y update
 apt-get -y install apache2
 apache2ctl configtest
-apt-get install -y php
+apt-get install -y php7
 sudo apt install php libapache2-mod-php
-apt-get install -y curl php-cli git
+apt-get install -y curl php7.0-cli git
 apt-get install -y wget
 
 curl -sS https://getcomposer.org/installer | php
