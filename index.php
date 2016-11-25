5a603ffd126d88cbf09e0d059233d1b99c60577a
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-25 01:38:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 echo "<h1>Hello</h1>";
 
-require_once 'vendor\autoload.php';
+require 'vendor\autoload.php';
 use WindowsAzure\Common\ServicesBuilder;
 use WindowsAzure\Common\ServiceException;
 use WindowsAzure\ServiceBus\Models\QueueInfo;
