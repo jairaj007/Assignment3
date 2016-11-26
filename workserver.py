1ba9e31906e4d4f85a0c7f695664f828244719cd
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-25 23:48:22 -0400

    Update workserver.py

--- workserver.py
+++ workserver.py
@@ -1,28 +1,21 @@
 # workserver.py - simple HTTP server with a do_work / stop_work API
 # GET /do_work activates a worker thread which uses CPU
 # GET /stop_work signals worker thread to stop
+from bottle import run, route
+from azure.servicebus import ServiceBusService, Message, Queue
+
 import math
 import socket
 import threading
 import time
 
-from azure.servicebus import ServiceBusService, Message, Queue
-from azure.servicebus import Message
-from bottle import run, route
-
-
 hostname = socket.gethostname()
 hostport = 9000
-
-
 
 def writebody():
 	body = '<html><head><title>Work interface - build</title></head>'
 	body += '<body><h2>Worker interface on</h2><ul><h3>'
 	return body
-	
-	
-
 	
 @route('/')
 def root():
