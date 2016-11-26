80711c6a3f4e1df306f36d9b2305703a459c4f98
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-26 17:09:57 -0400

    Update workserver.py

--- workserver.py
+++ workserver.py
@@ -11,20 +11,17 @@ from bottle import route, run
 hostname = socket.gethostname()
 hostport = 9000
 
-def writebody():
-   from azure.servicebus import ServiceBusService, Message, Queue
-   bus_read_service = ServiceBusService(
-       service_namespace='jairaj007',
-       shared_access_key_name='RootManageSharedAccessKey',
-       shared_access_key_value='lGSHGfaf8RBQ9lHbso85PCvGD2BCBVzpMKHHXLwluhg=')
-   msg = bus_service.receive_queue_message('myqueue', peek_lock=False)	
-   msg.delete()
-   return msg
-
 
 @route('/')
 def root():
-    return writebody()
+    from azure.servicebus import ServiceBusService, Message, Queue
+    bus_read_service = ServiceBusService(
+        service_namespace='jairaj007',
+        shared_access_key_name='RootManageSharedAccessKey',
+        shared_access_key_value='lGSHGfaf8RBQ9lHbso85PCvGD2BCBVzpMKHHXLwluhg=')
+    msg = bus_service.receive_queue_message('myqueue', peek_lock=True)	
+    msg.delete()
+    return msg
 
 
 run(host=hostname, port=hostport)
