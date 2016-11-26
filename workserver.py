cf23b459fe2b00fb87cb6be79a7ed46c14b1460b
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-26 18:21:19 -0400

    Update workserver.py

--- workserver.py
+++ workserver.py
@@ -15,8 +15,8 @@ hostport = 9000
 def writebody():
     bus_service = ServiceBusService(
         service_namespace='jairaj007',
-	shared_access_key_name='RootManageSharedAccessKey',
-	shared_access_key_value='lGSHGfaf8RBQ9lHbso85PCvGD2BCBVzpMKHHXLwluhg=')
+        shared_access_key_name='RootManageSharedAccessKey',
+        shared_access_key_value='lGSHGfaf8RBQ9lHbso85PCvGD2BCBVzpMKHHXLwluhg=')
     msg = bus_service.receive_queue_message('myqueue', peek_lock=True)	
     msg.delete()
     return(msg.body)
@@ -24,7 +24,7 @@ def writebody():
 	
 @route('/')
 def root():
-    return(writebody())
+    return writebody()
 
 
 run(host=hostname, port=hostport)
