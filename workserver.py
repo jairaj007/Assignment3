275650cbd8be9710cd9983da2fd54615e153325e
jairaj007 <jairaj007@users.noreply.github.com>
2016-11-29 13:41:39 -0400

    Update workserver.py

--- workserver.py
+++ workserver.py
@@ -3,6 +3,7 @@ import socket
 import threading
 import time
 
+from azure.servicebus import ServiceBusService, Message, Queue
 from bottle import run, route
 
 
@@ -10,7 +11,13 @@ hostname = socket.gethostname()
 hostport = 80
 
 def writebody():
-    return ("Hello")
+    bus_service = ServiceBusService(
+        service_namespace='jairaj007',
+        shared_access_key_name='RootManageSharedAccessKey',
+        shared_access_key_value='lGSHGfaf8RBQ9lHbso85PCvGD2BCBVzpMKHHXLwluhg=')
+    msg = bus_service.receive_queue_message('myqueue', peek_lock=True)	
+    msg.delete()
+    return msg.body
 
 	
 @route('/')
