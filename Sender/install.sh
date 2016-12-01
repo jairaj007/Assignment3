3330f43280eaa5886944383006a4610fa942c941
jairaj007 <jairaj007@users.noreply.github.com>
2016-12-01 15:26:31 -0400

    Create install.sh

---
+++ Sender/install.sh
@@ -0,0 +1,22 @@
+sudo apt-get -y update 
+sudo apt-get -y install apache2
+sudo apt-get install -y php5
+sudo apt-get install -y curl php5-cli git
+sudo apt-get install -y wget
+
+sudo curl -sS https://getcomposer.org/installer | php
+
+cd /var/www/html
+sudo wget https://raw.githubusercontent.com/jairaj007/Assignment3/master/Sender/index.php
+sudo wget https://raw.githubusercontent.com/jairaj007/Assignment3/master/Sender/sendMessages.php
+#if you need composer, enable these lines
+sudo wget https://raw.githubusercontent.com/jairaj007/Assignment3/master/composer.json
+sudo wget http://getcomposer.org/composer.phar
+sudo php composer.phar install
+sudo rm /var/www/html/index.html
+
+cd
+echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/servername.conf
+sudo a2enconf servername
+cd /var/www/html
+sudo service apache2 restart
