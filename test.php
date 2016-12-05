92861a7576e3eaf299378fa2c31c2fbbdfb411dd
jairaj007 <jairaj007@users.noreply.github.com>
2016-12-05 03:42:02 -0400

    Create test.php

---
+++ test.php
@@ -0,0 +1,27 @@
+require_once( 'Thread.php' );
+ 
+// test to see if threading is available
+if( ! Thread::available() ) {
+    die( 'Threads not supported' );
+}
+ 
+// function to be ran on separate threads
+function paralel( $_limit, $_name ) {
+    for ( $index = 0; $index &lt; $_limit; $index++ ) {
+        echo 'Now running thread ' . $_name . PHP_EOL;
+        sleep( 1 );
+    }
+}
+ 
+// create 2 thread objects
+$t1 = new Thread( 'paralel' );
+$t2 = new Thread( 'paralel' );
+ 
+// start them
+$t1->start( 10, 't1' );
+$t2->start( 10, 't2' );
+ 
+// keep the program running until the threads finish
+while( $t1->isAlive() && $t2->isAlive() ) {
+    sleep(1);
+}
